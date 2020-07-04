c51d019aaf4fe3a4acf60a0943ea396fe7ce11e4
Leah Rieger <leastuart92@gmail.com>
2020-07-04 17:48:01 -0500

    landing

--- client/components/nav-bar.vue
+++ client/components/nav-bar.vue
@@ -1,25 +1,15 @@
 <template>
 <div class="nav">
     <b-navbar toggleable="lg">
-        <b-nav align="left">
-
-            <a href="#" class="navbar-brand name router-link-exact-active router-link-active" target="_self">
-            <b-img src="./RL_logo.76a609ee.png" alt="Cartoon brain in a Jar">
-            </a>
-            <b-navbar-brand id="logo" to="/">Rieger Learning</b-navbar-brand>
-            <b-nav-item class="link" to="/">Home</b-nav-item>
-            <!--MOVE LOGIN TO A MODAL INSTEAD OF A WHOLE PAGE;
-            MAKE 2ND LINK A DIFF PAGE-->
-            <b-nav-item class="link" to="/login"> Login </b-nav-item>
-
-            <b-navbar-brand to="/" class="name"><img src="../Images/RL_logo.png" class="logo" alt="Cartoon Brain in a Jar">Rieger Learning</b-navbar-brand>
-        </b-nav>   
+          
         <b-nav align="right">
             <div class="col-6">
                 <b-nav-item to="/" active>Home</b-nav-item>
             </div>
             <div class="col-2">
                 <b-nav-item to="/login"> Login </b-nav-item>
+                 <!--MOVE LOGIN TO A MODAL INSTEAD OF A WHOLE PAGE;
+            MAKE 2ND LINK A DIFF PAGE-->
             </div>   
 
         </b-nav>
